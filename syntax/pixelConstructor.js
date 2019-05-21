a4bb9ceebc1bd0b6cf02da9fb344f34fc1409553
Evan Genest <genest.1@osu.edu>
2019-05-21 07:56:45 -0500

    Stub the PIXEL.PROTOTYPE.SHORTIFY() func.

--- syntax/pixelConstructor.js
+++ syntax/pixelConstructor.js
@@ -1,13 +1,35 @@
+//  This file is an example of
+// 1) constructor  2) 2-d array of pixel objects 3) prototype for the method
+// Reason: just kind of making it up as I go.  5/21/2019
+
+
+
+
 let pixel = function(r, g, b){
 	// allowed vals 0-255
 	this.red = r;
 	this.green = g;
 	this.blue = b;
-
 };
+
+// todo 1) write the math 2) figure out hexadecimal syntax in JS
+pixel.prototype.shortify = function(){
+	// returns a three digit HEX(!) number for color
+	// r, g, b -> #2
+}
 
 let WIDTH = 680, HEIGHT = 420;
 
+
+/**
+* @parameters: width and height of image
+*	@returns: 
+*
+*
+*
+*
+*
+*/
 let init = function(w, h){
-	
+
 }
