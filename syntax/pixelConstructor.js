69c7199204ff0b2c7084d54729c55157fea24ecb
Evan Genest <genest.1@osu.edu>
2019-05-21 07:43:36 -0500

    1st commit.

---
+++ syntax/pixelConstructor.js
@@ -0,0 +1,13 @@
+let pixel = function(r, g, b){
+	// allowed vals 0-255
+	this.red = r;
+	this.green = g;
+	this.blue = b;
+
+};
+
+let WIDTH = 680, HEIGHT = 420;
+
+let init = function(w, h){
+	
+}
